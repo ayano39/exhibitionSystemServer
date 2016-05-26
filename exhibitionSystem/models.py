6c81d5f3594cdd13f210503329d8fb91b46dc3b8
CCutlery <chenwd0803@163.com>
2016-05-26 20:32:28 +0800

    database created

--- exhibitionSystem/models.py
+++ exhibitionSystem/models.py
@@ -2,4 +2,35 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class user(models.Model):
+    uid = models.CharField(max_length=10)
+
+class boothInfo(models.Model):
+    booth = models.CharField(max_length=30)
+    coordx = models.IntegerField(default=0)
+    coordy = models.IntegerField(default=0)
+    info = models.TextField(max_length=300)
+
+class user_theme(models.Model):
+    uid = models.CharField(max_length=10)
+    medical = models.IntegerField(default=0)
+    vehicle = models.IntegerField(default=0)
+    home = models.IntegerField(default=0)
+    industry = models.IntegerField(default=0)
+    wear = models.IntegerField(default=0)
+
+class booth_theme(models.Model):
+    booth = models.CharField(max_length=30)
+    medical = models.IntegerField(default=0)
+    vehicle = models.IntegerField(default=0)
+    home = models.IntegerField(default=0)
+    industry = models.IntegerField(default=0)
+    wear = models.IntegerField(default=0)
+
+class user_booth(models.Model):
+    uid = models.CharField(max_length=10)
+    booth = models.CharField(max_length=30)
+    reached = models.IntegerField(default=0)
+    stayTime = models.IntegerField(default=0)
+
 # Create your models here.
