ea61f77076079dc1dda2212435b1acf240e40c83
Aries-my <my_zhu@outlook.com>
2016-06-11 16:52:43 +0800

    implement "update_trace" view

--- exhibitionSystem/models.py
+++ exhibitionSystem/models.py
@@ -19,6 +19,9 @@ class user_theme(models.Model):
     industry = models.IntegerField(default=0)
     wear = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.uid
+
 class booth_theme(models.Model):
     booth = models.CharField(max_length=30)
     medical = models.IntegerField(default=0)
@@ -27,10 +30,12 @@ class booth_theme(models.Model):
     industry = models.IntegerField(default=0)
     wear = models.IntegerField(default=0)
 
+
 class user_booth(models.Model):
     uid = models.CharField(max_length=10)
     booth = models.CharField(max_length=30)
     reached = models.IntegerField(default=0)
     stayTime = models.IntegerField(default=0)
 
+#hh
 # Create your models here.
--- exhibitionSystem/views.py
+++ exhibitionSystem/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from exhibitionSystem.models import *
 
 # Create your views here.
 
@@ -13,8 +15,30 @@ from django.shortcuts import render
 #   4.将reached字段设为1， save这条记录
 #   5.stayTime字段的更新将在下一轮迭代中完成~
 #   6.调用update_intvec方法
+
+@csrf_exempt
 def update_trace(request):
-    pass
+    userid = request.POST.get("uid")
+    boothid = request.POST.get("boothId")
+    if(userid == None or boothid == None):
+        return ""
+    try:
+        userBooth = user_booth.objects.get(uid = userid , booth = boothid);
+    except Exception as ex:
+        reached = 1
+        userBooth = user_booth()
+        userBooth.uid = userid
+        userBooth.booth = boothid
+        userBooth.reached = reached
+        userBooth.save()
+    else:
+        userBooth.uid = userid
+        userBooth.booth = boothid
+        reached = userBooth.reached
+        userBooth.reached = reached + 1
+        userBooth.save()
+    update_intvec(request)
+    return
 
 #该方法的触发时机：影响用户兴趣向量的行为发生时
 #该方法的两种思路:
