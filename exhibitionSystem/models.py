3f413cd35f928c8d4d677de30ef93822fb8a3131
AlsikeE <771845783@qq.com>
2016-06-11 11:32:26 +0800

    table booth_theme deleted, table booth_info modified, table booth_conn added

--- exhibitionSystem/models.py
+++ exhibitionSystem/models.py
@@ -7,8 +7,11 @@ class user(models.Model):
 
 class boothInfo(models.Model):
     booth = models.CharField(max_length=30)
-    coordx = models.IntegerField(default=0)
-    coordy = models.IntegerField(default=0)
+    medical = models.IntegerField(default=0)
+    vehicle = models.IntegerField(default=0)
+    home = models.IntegerField(default=0)
+    industry = models.IntegerField(default=0)
+    wear = models.IntegerField(default=0)
     info = models.TextField(max_length=300)
 
 class user_theme(models.Model):
@@ -19,13 +22,10 @@ class user_theme(models.Model):
     industry = models.IntegerField(default=0)
     wear = models.IntegerField(default=0)
 
-class booth_theme(models.Model):
-    booth = models.CharField(max_length=30)
-    medical = models.IntegerField(default=0)
-    vehicle = models.IntegerField(default=0)
-    home = models.IntegerField(default=0)
-    industry = models.IntegerField(default=0)
-    wear = models.IntegerField(default=0)
+class booth_conn(models.Model):
+    B1 = models.CharField(max_length=30)
+    B2 = models.CharField(max_length=30)
+
 
 class user_booth(models.Model):
     uid = models.CharField(max_length=10)
