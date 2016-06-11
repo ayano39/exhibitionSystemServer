e3203a083783719321358409ca7968b43a4f7792
CCutlery <chenwd0803@163.com>
2016-06-11 17:13:22 +0800

    tabel boothInfo modified

--- exhibitionSystem/models.py
+++ exhibitionSystem/models.py
@@ -6,13 +6,13 @@ class user(models.Model):
     uid = models.CharField(max_length=10)
 
 class boothInfo(models.Model):
-    booth = models.CharField(max_length=30)
+    booth = models.CharField(max_length=30,default='')
     medical = models.IntegerField(default=0)
     vehicle = models.IntegerField(default=0)
     home = models.IntegerField(default=0)
     industry = models.IntegerField(default=0)
     wear = models.IntegerField(default=0)
-    info = models.TextField(max_length=300)
+    info = models.TextField(max_length=300,default='')
 
 class user_theme(models.Model):
     uid = models.CharField(max_length=10)
@@ -25,7 +25,6 @@ class user_theme(models.Model):
 class booth_conn(models.Model):
     B1 = models.CharField(max_length=30)
     B2 = models.CharField(max_length=30)
-
 
 class user_booth(models.Model):
     uid = models.CharField(max_length=10)
