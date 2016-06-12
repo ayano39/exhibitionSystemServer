85a2e4072d3aaa3f5596d5867b08ab6d59fcc480
CCutlery <chenwd0803@163.com>
2016-06-12 23:37:26 +0800

    test cases

--- exhibitionSystem/tests.py
+++ exhibitionSystem/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+import unittest
+from unittest import TestCase
+from exhibitionSystem.views import *
 
+class test_views(TestCase):
+    def test_makeroute(self):
+        interests=[1,1,1,1,1]
+        id = 1
+        a = make_route(1,interests)
+        print(a.grade)
+        print(a.path)
+
+if __name__ == '__main__':
+    unittest.main()
 # Create your tests here.
